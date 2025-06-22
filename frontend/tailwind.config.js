3dd81f3e42ac9516eed14e1cef2b0df97b980758
Akash Singa <akashsinga@gmail.com>
2025-06-22 20:48:58 +0530

    tailwind config update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,scss}"],
+    darkMode: 'selector',
     theme: {},
     plugins: [],
     safelist: {}
